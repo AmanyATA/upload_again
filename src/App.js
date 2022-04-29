aca16abc1ef036c11b628f99e107974ae4970c0a
amany <tamany261@gmail.com>
2022-04-29 19:01:55 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import UseEffectAPI from './fetshAPI/useEffectAPI';
+
+
+function App() {
+
+  return (
+<div className="App">
+     <UseEffectAPI/>
+    </div>
+  )
+}
+export default App
